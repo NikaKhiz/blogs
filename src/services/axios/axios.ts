b3e6ddf0e54517ee9ff0bff46284a170f1b10a08
NikaKhiz <khizambarelinika123@gmail.com>
2023-12-27 15:53:50 +0400

    fix: axios base url

--- src/services/axios/axios.ts
+++ src/services/axios/axios.ts
@@ -4,7 +4,7 @@ const ACCESS_TOKEN =
   'bc65aa9d77ec2065670e6d12c18a7b1e5f201a5e659db454156aab555fadc3b7';
 
 const axiosInstance = axios.create({
-  baseURL: '',
+  baseURL: 'https://api.blog.redberryinternship.ge/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
